0ea658f4661b530179d2bc9ece9e98e7a151d03b
kiah.jones <kiah.jones@docusign.com>
2017-08-10 14:54:26 -0700

    GOJ-1: Committing what will be known as the first revision of my homepage

---
+++ index.js
@@ -0,0 +1,44 @@
+'use strict';
+
+var app = angular.module('app',[]);
+
+app.controller('MainController', ['$scope', '$window', function($scope, $window) {
+    $scope.logResize = function () {
+        // why is this needed for directive?
+    };
+
+    angular.element($window).on('resize', function () {
+        console.log($window.innerWidth);
+        resizeCards();
+    });
+
+    var resizeCards = function() {
+        var heightArray = [$('#card-1').height(), $('#card-2').height(), $('#card-3').height()]
+        var maxHeight = Math.max.apply(null, heightArray)
+
+        var widthArray = [$('#card-1').width(), $('#card-2').width(), $('#card-3').width()]
+        var minWidth = Math.min.apply(null, widthArray)
+
+        $('.card').height(maxHeight)
+        $('.card').width(minWidth)
+    }
+
+    resizeCards();
+}]);
+
+app.directive('cardResize', ['$window', function ($window) {
+
+     return {
+        link: link,
+        restrict: 'E'            
+     };
+
+     function link(scope, element, attrs){
+
+       angular.element($window).bind('resize', function(){
+            var array = [$('#card-1').height(), $('#card-2').height(), $('#card-3').height()]
+            var maxHeight = Math.max.apply(null, array)
+            $('.card').height(maxHeight)
+       });    
+     }    
+ }]);
